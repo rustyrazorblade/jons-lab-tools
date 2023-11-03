a046437c303633dde76118ca1b3866ae51226203
Jon Haddad <jon@jonhaddad.com>
2023-11-03 14:59:31 -0700

    Might work but haven't tested the sensor at the end

--- dags/cassandra/provision-cassandra.py
+++ dags/cassandra/provision-cassandra.py
@@ -8,6 +8,8 @@ from airflow.providers.amazon.aws.hooks.ec2 import EC2Hook
 
 import datetime
 
+from airflow.sensors.base import PokeReturnValue
+
 DAG_NAME = "provision-cassandra-cluster"
 
 dag_params = {
@@ -69,27 +71,29 @@ def provision_cassandra_cluster():
             Description='My security group',
             VpcId=vpc_id
         )
-        security_group_id = sg_response.id
-        print(f"Created Security Group: {security_group_id}")
+        print(f"Created Security Group: {sg_response}")
+        return sg_response.id
 
-        # Add a rule to allow SSH access (optional)
-        ec2.authorize_security_group_ingress(
+    @task()
+    def authorize_ingress(security_group_id, params=None):
+        ec2 = EC2Hook(aws_conn_id="aws_default", region_name=params["region"]).conn
+        security_group = ec2.SecurityGroup(security_group_id)
+        return security_group.authorize_ingress(
             GroupId=security_group_id,
             IpProtocol='tcp',
             FromPort=22,
             ToPort=22,
-            CidrIp='0.0.0.0/0'
-        )
-        return security_group_id
+            CidrIp='0.0.0.0/0')
 
-    @task(multiple_outputs=True)
+    @task()
     def create_ec2_instances(subnet_id, security_group_id, params=None):
+        num = params["number_of_instances"]
         ec2 = EC2Hook(aws_conn_id="aws_default", region_name=params["region"]).conn
         instances = ec2.create_instances(
-            ImageId='ami-12345',  # Replace with a valid AMI ID
-            MinCount=params.number_of_instances,
-            MaxCount=params.number_of_instances,
-            InstanceType='t2.micro',
+            ImageId='ami-03d390062ea11f660',  # Replace with a valid AMI ID
+            MinCount=num,
+            MaxCount=num,
+            InstanceType=params["instance_type"],
             SecurityGroupIds=[security_group_id],
             SubnetId=subnet_id,
             TagSpecifications=[{
@@ -99,14 +103,25 @@ def provision_cassandra_cluster():
         )
         instance_ids = [instance.id for instance in instances]
         print(f"Created EC2 Instances: {instance_ids}")
-        return {
-            'instance_ids': instance_ids
-        }
+        return instance_ids
+
+    @task.sensor(poke_interval=30, timeout=600, mode="reschedule")
+    def wait_for_instances(instances, params=None):
+        ec2 = EC2Hook(aws_conn_id="aws_default", region_name=params["region"]).conn
+
+        for instance_id in instances:
+            instance = ec2.Instance(instance_id)
+            if instance.state["Code"] != 16:  # 16 is running, 0 = pending
+                return PokeReturnValue(is_done=False)
+
+        return PokeReturnValue(is_done=True)
 
     vpc = create_vpc()
     subnet_id = create_subnet(vpc)
     security_group_id = create_security_group(vpc)
-    create_ec2_instances(subnet_id, security_group_id)
+    authorize_ingress(security_group_id)
+    instances = create_ec2_instances(subnet_id, security_group_id)
+    wait_for_instances(instances)
 
 
 my_tag = provision_cassandra_cluster()
