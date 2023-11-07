526b43de8ed66b83c3240154dc283a14a32cf4d0
Jon Haddad <jon@jonhaddad.com>
2023-11-06 23:48:05 -0800

    new dag that creates instances in an existing VPC

--- dags/cassandra/provision-cassandra.py
+++ dags/cassandra/provision-cassandra.py
@@ -10,7 +10,7 @@ import datetime
 
 from airflow.sensors.base import PokeReturnValue
 
-from globals import JLT_VPC_LIST, JLT_DEFAULT_REGION, JLT_DEFAULT_VPC
+from globals import JLT_VPC_LIST, JLT_DEFAULT_REGION, JLT_DEFAULT_VPC, JLT_DEFAULT_BASE_AMI
 
 DAG_NAME = "provision-cassandra-cluster"
 
@@ -25,7 +25,7 @@ dag_params = {
                                "c5d.xlarge": "c5.xlarge (8GB, 4CPU, 1x100GB NVMe)",
                            }),
     "region": JLT_DEFAULT_REGION,
-    "number_of_instances": Param(3, type="integer", minimum=1, maximum=30),
+    "number_of_instances": Param(3, type="integer", enum=[3, 6, 9, 12]),
     "vpc": Param(default_vpc, enum=vpcs)
 }
 
@@ -37,76 +37,54 @@ dag_params = {
      params=dag_params)
 def provision_cassandra_cluster():
     # new task to create vpc
-    @task()
-    def create_vpc(params=None):
-        region_name = params["region"]
-        cluster_name = params["cluster_name"]
-
-        # boto create vpc
-        ec2 = EC2Hook(aws_conn_id="aws_default", region_name=region_name).conn
-        vpc_id = ec2.create_vpc(
-            CidrBlock="10.0.0.0/16"
-        ).id
-
-        ec2.create_tags(
-            Resources=[vpc_id],
-            Tags=[{'Key': 'Name', 'Value': cluster_name}]
-        )
-
-        return vpc_id
-
-    # taskflow task that creates a VPC subnet
-    @task()
-    def create_subnet(vpc_id: str, params=None):
-        region_name = params["region"]
-        ec2 = EC2Hook(aws_conn_id="aws_default", region_name=region_name).conn
-        subnet = ec2.create_subnet(
-            CidrBlock="10.0.0.0/16",
-            VpcId=vpc_id
-        )
-        return subnet.id
-
     # new task to create security group
+
+    @task
+    def get_vpc(params=None):
+        region_name = params["region"]
+        vpc_name = params["vpc"]
+        ec2 = EC2Hook(aws_conn_id="aws_default", region_name=region_name).conn
+        filters = [{'Name': 'tag:Name', 'Values': [vpc_name]}]
+        vpc = list(ec2.vpcs.filter(Filters=filters))[0]
+        return vpc.id
+
     @task()
-    def create_security_group(vpc_id: str, params=None):
+    def get_subnets(vpc_id, params=None):
+        # get all the subnets in the vpc
         region_name = params["region"]
         ec2 = EC2Hook(aws_conn_id="aws_default", region_name=region_name).conn
-
-        sg_response = ec2.create_security_group(
-            GroupName='my_security_group',
-            Description='My security group',
-            VpcId=vpc_id
-        )
-        print(f"Created Security Group: {sg_response}")
-        return sg_response.id
+        vpc = ec2.Vpc(vpc_id)
+        subnets = vpc.subnets.all()
+        return [subnet.id for subnet in subnets]
 
     @task()
-    def authorize_ingress(security_group_id, params=None):
-        ec2 = EC2Hook(aws_conn_id="aws_default", region_name=params["region"]).conn
-        security_group = ec2.SecurityGroup(security_group_id)
-        return security_group.authorize_ingress(
-            GroupId=security_group_id,
-            IpProtocol='tcp',
-            FromPort=22,
-            ToPort=22,
-            CidrIp='0.0.0.0/0')
+    def get_security_group(vpc_id, params=None):
+        region_name = params["region"]
+        ec2 = EC2Hook(aws_conn_id="aws_default", region_name=region_name).conn
+        vpc = ec2.Vpc(vpc_id)
+        logging.info(f"Fetching security groups for {vpc}")
+        vps_security_groups = list(vpc.security_groups.all())
+        logging.info(f"SGs fetched {vps_security_groups}")
+        return vps_security_groups[0].id
 
     @task()
-    def create_ec2_instances(subnet_id, security_group_id, params=None):
-        num = params["number_of_instances"]
+    def create_ec2_instances(subnet, security_group_id, params=None):
+        # spread the instances across the subnets
         ec2 = EC2Hook(aws_conn_id="aws_default", region_name=params["region"]).conn
+        num = int(params["number_of_instances"] / 3)
+
         instances = ec2.create_instances(
-            ImageId='ami-03d390062ea11f660',  # Replace with a valid AMI ID
+            ImageId=JLT_DEFAULT_BASE_AMI,  # Replace with a valid AMI ID
             MinCount=num,
             MaxCount=num,
             InstanceType=params["instance_type"],
             SecurityGroupIds=[security_group_id],
-            SubnetId=subnet_id,
+            SubnetId=subnet,
             TagSpecifications=[{
                 'ResourceType': 'instance',
                 'Tags': [{'Key': 'Name', 'Value': 'MyInstance'}]
-            }]
-        )
+            }])
+
         instance_ids = [instance.id for instance in instances]
         print(f"Created EC2 Instances: {instance_ids}")
         return instance_ids
@@ -122,12 +100,11 @@ def provision_cassandra_cluster():
 
         return PokeReturnValue(is_done=True)
 
-    vpc = create_vpc()
-    subnet_id = create_subnet(vpc)
-    security_group_id = create_security_group(vpc)
-    authorize_ingress(security_group_id)
-    instances = create_ec2_instances(subnet_id, security_group_id)
-    wait_for_instances(instances)
+    vpc = get_vpc()
+    subnets = get_subnets(vpc)
+    sg = get_security_group(vpc)
+    instances = create_ec2_instances.partial(security_group_id=sg).expand(subnet=subnets)
+    wait_for_instances.expand(instances=instances)
 
 
 my_tag = provision_cassandra_cluster()
--- dags/globals.py
+++ dags/globals.py
@@ -1,3 +1,4 @@
 JLT_VPC_LIST = "jlt_vpc_list"
 JLT_DEFAULT_VPC = "jlt"
 JLT_DEFAULT_REGION = "us-west-2"
+JLT_DEFAULT_BASE_AMI = 'ami-03d390062ea11f660'
